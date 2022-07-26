77003e798abb10a0edc114a6f974640e663f9ec3
felipe2640 <felipe_brito09@hotmail.com>
2022-07-26 09:40:11 -0300

    first commit

---
+++ src/App.ts
@@ -0,0 +1,33 @@
+import express, { Application } from 'express';
+
+export default class App {
+  public app: Application;
+  public port: number;
+
+  constructor(appConfig: { port: number; middlewares: any; controllers: any }) {
+    this.app = express();
+    this.port = appConfig.port;
+    this.setMiddlewares(appConfig.middlewares);
+    this.setControllers(appConfig.controllers);
+  }
+
+  public listen() {
+    this.app.listen(this.port, () => console.log('Express has been started'));
+  }
+
+  private setMiddlewares(middlewares: {
+    forEach: (mid: (middleware: any) => void) => void;
+  }) {
+    middlewares.forEach((middleware) => {
+      this.app.use(middleware);
+    });
+  }
+
+  private setControllers(controllers: {
+    forEach: (con: (controller: any) => void) => void;
+  }) {
+    controllers.forEach((controller) => {
+      this.app.use('/', controller.router);
+    });
+  }
+}
---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import App from './App';
+import * as bodyParser from 'body-parser';
+import morgan from 'morgan';
+
+import HomeController from './controllers/HomeController';
+
+const app = new App({
+  port: 3000,
+  middlewares: [
+    morgan('dev'),
+    bodyParser.urlencoded({ extended: false }),
+    bodyParser.json()
+  ],
+  controllers: [new HomeController()]
+});
+
+app.listen();

fb5a537b8520b8058f2bcf61b0601d5ce962a163
felipe2640 <felipe_brito09@hotmail.com>
2022-07-28 23:37:34 -0300

    feat(Create user db): Create user wiht jwt

--- src/models/task.model.ts
+++ src/models/task.model.ts
@@ -1,4 +1,6 @@
 export interface IUser {
   email: string;
   name?: string;
+  password: string;
+  token?: string;
 }
--- src/services/task.services.ts
+++ src/services/task.services.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from 'express';
+import bcrypt from 'bcrypt';
+import { sign } from 'jsonwebtoken';
 
 import { IUser } from '../models/task.model';
 import { prisma } from './prisma.service';
@@ -18,9 +20,32 @@ export default class TaskService {
   }
 
   public async create(req: Request, res: Response) {
-    const iUser = req.body as IUser;
-    const user = await prisma.user.create({ data: iUser });
+    const jwtToken =
+      '1ed4e67233e04a70f30bce6ffb2d2893c5d7942fdc0f21ef2305c9bbc37d3a057558a9';
 
-    return res.json({ user });
+    try {
+      const { email, name, password: pass } = req.body as IUser;
+
+      // Validate user input
+      if (!(email && pass && name)) {
+        res.status(400).send('All input is required');
+      }
+
+      //Encrypt user password
+      const password = await bcrypt.hash(pass, 10);
+
+      const token = sign({ user_id: email }, jwtToken, {
+        expiresIn: '2h'
+      });
+
+      const user = { email, name, password, token };
+      await prisma.user.create({ data: user });
+      const prismamsg = prisma.$use(async (params, next) => {
+        return next(params);
+      });
+      res.json({ msg: prismamsg });
+    } catch (err) {
+      console.error(err);
+    }
   }
 }
